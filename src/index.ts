90abfd61947c31830b1529008539fc927b02524e
rohansx <sharmarohan615@gmail.com>
2024-02-12 00:27:24 +0530

    Created generate function

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import simpleGit from "simple-git";
+import { generate } from "./utils";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// POSTMAN
+app.post("/deploy", async (req, res) => {
+  const repoUrl = req.body.repoUrl;
+  const id = generate(); // asd12
+  await simpleGit().clone(repoUrl, `output/${id}`);
+
+  res.json({
+    id: id,
+  });
+});
+
+app.listen(3000);

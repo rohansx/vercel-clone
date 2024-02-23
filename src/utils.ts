9f4ad04eaae4f1fb604ddb4fc182b9b4f9732c29
rohansx <sharmarohan615@gmail.com>
2024-02-23 16:48:08 +0530

    feat: clone the git repo and execute the cmd on docker instance

--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-export function generate() {
-  const subset = "123456789qwertyuiopasdfghjklzxcvbnm";
-  const length = 5;
-  var id = "";
-  for (let i = 0; i < length; i++) {
-    id += subset[Math.floor(Math.random() * subset.length)];
-  }
-  return id;
-}

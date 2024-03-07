ac2299a6d9ce000c5a669c8b39568d2481fbd672
rohansx <sharmarohan615@gmail.com>
2024-03-07 23:52:54 +0530

    feat: added frontend and fix project structure

--- upload-service/src/index.ts
+++ upload-service/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import simpleGit from "simple-git";
 import { generate } from "./utils";
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.json());
 // POSTMAN
 app.post("/deploy", async (req, res) => {
   const repoUrl = req.body.repoUrl;
+  await simpleGit().clone(repoUrl, `./${generate()}`);
 });
 
 app.listen(3000);
